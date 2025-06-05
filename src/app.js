02ad472aff6afb5d850340500a8c5f9b93320aec
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-06-04 20:31:46 -0400

    Multiple Route handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const express=require('express');
 
 const app=express()
 
+
+
+app.get("/user/", (req, res, next)=>{
+    console.log(req.params)
+    next()
+
+}, (req, res,next)=>{
+    console.log("2nd route")
+    res.send("this is the second route");
+})
 app.use("/test", (req, res)=>{
     res.send("HEllo from  test server")
 })
