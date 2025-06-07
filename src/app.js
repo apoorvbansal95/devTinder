6d960958f235ed926659420fa0a2ecfdb40c1c90
apoorvbansal95 <apoorv95.ab@gmail.com>
2025-06-07 15:51:23 -0400

    dynamic sign up

--- src/app.js
+++ src/app.js
@@ -3,17 +3,23 @@ const UserModel=require('./models/user')
 const ConnectDB=require('./config/database')
 const app=express()
 
- app.post("/signup", async (req, res)=>{
-    const userObj ={
-        firstName:"Ram",
-        lastName:"Singh",
-        emailId:"rsingh@gmail.com",
-        password:"1234@aa"
+app.use(express.json())
 
-    }
+
+ app.post("/signup", async (req, res)=>{
+
+    console.log(req.body);
+
+    // const userObj ={
+    //     firstName:"Ram",
+    //     lastName:"Singh",
+    //     emailId:"rsingh@gmail.com",
+    //     password:"1234@aa"
+
+    // }
 
     // Create a new user instance
-    const newUser= new UserModel(userObj)
+    const newUser= new UserModel(req.body)
      await newUser.save()
      res.send("User created successfully");
  })
